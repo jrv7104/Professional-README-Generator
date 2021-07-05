b78b3918adbb8b2b939ee1483261127fe71abf86
Jason Viox <jrv04@hotmail.com>
2021-07-05 14:23:05 -0500

    add two questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,26 @@ const inquirer = require('inquirer');
 const { fstat } = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ( 
+    [
+        {
+            type: 'input',
+            message="What is the title of your project?",
+            name: 'title',
+        }
+        {
+            type:'input',
+            message:'how do you install your app?',
+            name:'installation',
+        }
+        {
+            type:'input',
+            message:'?',
+            name:'installation',
+        }
+
+]
+)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
