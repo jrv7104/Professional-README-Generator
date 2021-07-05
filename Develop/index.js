da6164400c89e6e863046598be7661833fe162f3
Jason Viox <jrv04@hotmail.com>
2021-07-05 14:28:35 -0500

    add punctuation, license and github username

--- Develop/index.js
+++ Develop/index.js
@@ -9,22 +9,34 @@ const questions = (
             type: 'input',
             message="What is the title of your project?",
             name: 'title',
-        }
+        },
         {
             type:'input',
             message:'how do you install your app?',
             name:'installation',
-        }
+        },
         {
             type:'input',
             message:'What are your instructions for using the app?',
             name:'instructions',
-        }
+        },
         {
             type:'input',
             message:'Who is credited for the function and creativity of the app?',
             name:'credits',
-        }
+        },
+        {
+            type:'list',
+            message:'What license did you use?',
+            name:'credits',
+            choices:['MIT', 'GPL', 'Apache', 'GNU', 'Other', 'N/A'],
+        },
+        {
+            type:'input',
+            message:'What is your GitHub username?'
+            name:'githubname'
+        },
+        
 
 ]
 )
