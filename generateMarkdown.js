5cac37c9c9aaa380633fd752a74bd15223ea4ce9
Jason Viox <jrv04@hotmail.com>
2021-07-08 10:29:53 -0500

    add license badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -11,9 +11,17 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {
   if(license == "Apache") {
     return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  } else if(license == "MIT"){
-    return `something`
-  }else {
+  } 
+  else if(license == "MIT"){
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+  }
+  else if (license == "GPL") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if (license == "GNU") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else {
     return `no license provided or license outside of list`
   }
 }
